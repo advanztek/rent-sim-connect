f525b738c7f7885c6336dac23e9096076c8bce1f
Kingsley <kingsleyterwase5@gmail.com>
2025-02-10 13:30:31 +0100

    Update index.jsx

--- src/component/VideoComponent/index.jsx
+++ src/component/VideoComponent/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Box from '@mui/material/Box'
 import IconButton from '@mui/material/IconButton'
 import PlayArrowOutlinedIcon from '@mui/icons-material/PlayArrowOutlined'
@@ -7,8 +7,7 @@ const styles = {
   container: {
     position: 'relative',
     width: '100%',
-    height: '100%',
-    minHeight: '380px',
+    height: '380px',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
@@ -16,18 +15,11 @@ const styles = {
     overflow: 'hidden',
     borderRadius: '30px'
   },
-  iframeWrapper: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%'
-  },
-  iframe: {
+  video: {
     width: '100%',
     height: '100%',
-    border: 'none',
-    display: 'block'
+    objectFit: 'cover',
+    borderRadius: '30px'
   },
   overlay: {
     position: 'absolute',
@@ -48,63 +40,40 @@ const styles = {
     padding: '2rem',
     backgroundColor: '#000',
     opacity: 0.3
-  },
-  thumbnail: {
-    width: '100%',
-    height: '100%',
-    objectFit: 'cover',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    borderRadius: '30px'
   }
 }
 
 const VideoComponent = ({ videoSrc, autoPlay = false }) => {
   const [isPlaying, setIsPlaying] = useState(autoPlay)
 
-  useEffect(() => {
-    if (autoPlay) {
-      setIsPlaying(true)
-    }
-  }, [autoPlay])
-
-  const getEmbeddedUrl = (url) => {
-    if (url.includes('youtube.com') || url.includes('youtu.be')) {
-      const videoId = url.split('v=')[1]?.split('&')[0] || url.split('/').pop();
-      return `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&loop=1&playlist=${videoId}&controls=0&disablekb=1&modestbranding=1&showinfo=0&rel=0`;
-    }
-    return url;
-  };
-  
-
-  const getThumbnailUrl = (url) => {
-    if (url.includes('youtube.com') || url.includes('youtu.be')) {
-      const videoId = url.split('v=')[1]?.split('&')[0] || url.split('/').pop()
-      return `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`
-    }
-    return ''
-  }
-
   return (
     <Box sx={styles.container}>
       {isPlaying ? (
-        <Box sx={styles.iframeWrapper}>
-          <iframe
-            src={getEmbeddedUrl(videoSrc)}
-            allow="autoplay; encrypted-media"
-            allowFullScreen
-            style={styles.iframe}
-            
-          />
-          <script src="https://player.vimeo.com/api/player.js"></script>
-        </Box>
+        <video
+          src={videoSrc}
+          autoPlay={autoPlay}
+          muted={autoPlay}
+          controls={!autoPlay}
+          style={styles.video}
+          onEnded={() => setIsPlaying(false)}
+        />
       ) : (
-        <>
-          <img src={getThumbnailUrl(videoSrc)} alt='Video Thumbnail' style={styles.thumbnail} />
+          <>
+            
+          <img
+            src={`${videoSrc}#t=0.1`}
+            alt='Video Thumbnail'
+            style={{ ...styles.video, position: 'absolute', top: 0, left: 0 }}
+          />
           <Box sx={styles.overlay} />
-          <IconButton color='primary' onClick={() => setIsPlaying(true)} sx={styles.playButton}>
-            <PlayArrowOutlinedIcon sx={{ fontSize: '80px', color: '#FFFFFF', borderRadius: '50%' }} />
+          <IconButton
+            color='primary'
+            onClick={() => setIsPlaying(true)}
+            sx={styles.playButton}
+          >
+            <PlayArrowOutlinedIcon
+              sx={{ fontSize: '80px', color: '#FFFFFF', borderRadius: '50%' }}
+            />
           </IconButton>
         </>
       )}
